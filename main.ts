d09383297b07b101e0de28f428a395a9d6f79073
Nik Kita <nik-kita@on.github>
2024-11-24 04:25:28 +0100

    📦 NEW: Add XState integration for CLI prompts and implement Zod validation

--- main.ts
+++ main.ts
@@ -1,4 +1,21 @@
 import type { NodePlopAPI } from "plop";
+import { createActor } from "xstate";
+import { machine } from "./cli.ts";
+
+const cli = createActor(machine);
+
+const cli_output = await new Promise((resolve, reject) => {
+  cli.start();
+  cli.subscribe((snapshot) => {
+    if (snapshot.status === "done") {
+      resolve(snapshot.output);
+    } else if (snapshot.status === "error") {
+      reject(snapshot.error);
+    }
+  });
+});
+
+console.log(cli_output);
 
 export default function (plop: NodePlopAPI) {
 }
