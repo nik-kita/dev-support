de14493c32c19ea677893911a281e06b0192ec28
Nik Kita <nik-kita@on.github>
2024-11-25 03:26:09 +0100

    👌 IMPROVE: Implement custom template generator and save functionality in CLI

--- generators/gen_file_by_custom_template.ts
+++ generators/gen_file_by_custom_template.ts
@@ -1,4 +1,4 @@
-import { NodePlopAPI } from "plop";
+import { ActionType, NodePlopAPI } from "plop";
 import { OutputCliMachine } from "../cli/types/output.ts";
 
 export async function gen_file_by_custom_template(
@@ -10,6 +10,7 @@ export async function gen_file_by_custom_template(
   >,
 ) {
   await Deno.mkdir(".output", { recursive: true });
+  const originalContent = options.content;
   for (const [k, v] of Object.entries(options.dictionary)) {
     options.content = options.content.replaceAll(k, `{{${v}}}`);
   }
@@ -17,22 +18,66 @@ export async function gen_file_by_custom_template(
   await Deno.writeTextFile(path, options.content);
   plop.setGenerator("gen_file_by_custom_template", {
     description: "Generate file based on custom blueprint",
-    prompts: Object.entries(options.dictionary).map(([k, v]) => {
-      return {
+    prompts: [
+      ...Object.entries(options.dictionary).map(([k, v]) => {
+        return {
+          type: "input",
+          name: v,
+          message: `Enter value for ${k}`,
+        };
+      }),
+      {
         type: "input",
-        name: v,
-        message: `Enter value for ${k}`,
-      };
-    }),
+        name: "__save__",
+        message: "Name for this new template (skip to not save):",
+      },
+    ],
     actions(answers) {
-      return [
+      const { __save__, ...data } = answers || {};
+      const actions: ActionType[] = [
         {
           type: "add",
           path: path.substring(0, path.length - ".hbs".length),
           templateFile: path,
-          data: answers,
+          data,
         },
       ];
+
+      if (__save__) {
+        /**
+         * @description Save this template for future use
+         */
+        actions.push({
+          type: "add",
+          path: `templates/${__save__}.ts.hbs`,
+          template: originalContent,
+        });
+        /**
+         * @description Generate generator for this template
+         */
+        actions.push(
+          {
+            type: "add",
+            path: `generators/gen_${__save__}.ts`,
+            templateFile: `templates/custom_generator.ts.hbs`,
+            data: {
+              name: __save__,
+              description: `Generate ${__save__}`,
+            },
+            transform(content: string) {
+              const transformedContent = content
+                .replaceAll(/^\s+\/\/ dd_RM_LINE_bb\s+$/g, "")
+                .replaceAll(/^\s+\/\/ dd_REPLACE_LINE_bb/g, "")
+                .replace("// dd_options_bb", "// TODO options")
+                .replace("// dd_prompts_bb", "// TODO prompts");
+
+              return transformedContent;
+            },
+          },
+        );
+      }
+
+      return actions;
     },
   });
 }
